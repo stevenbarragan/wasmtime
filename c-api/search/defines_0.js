8f3501cc0fa3dc59d262e9f3a5cd03eb282cbca5
Deploy from CI <>
2020-12-11 19:28:55 +0000

    Deploy 1d90c329b40303437f8a4b14a7f8844a0822af5c to gh-pages

---
+++ c-api/search/defines_0.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['wasm_5fextern_5finstance_1007',['WASM_EXTERN_INSTANCE',['../wasmtime_8h.html#a3bdd64f8a45faedcdb5a01a8629cc7d0',1,'wasmtime.h']]],
+  ['wasm_5fextern_5fmodule_1008',['WASM_EXTERN_MODULE',['../wasmtime_8h.html#adeb319ce1df5e395a2dcd8a6d0421bd2',1,'wasmtime.h']]],
+  ['wasm_5fname_1009',['wasm_name',['../wasm_8h.html#a353fb6d1224a21e2b47257548522bef4',1,'wasm.h']]],
+  ['wasm_5fname_5fcopy_1010',['wasm_name_copy',['../wasm_8h.html#abf49658a4a1b2279827e907da13f51f2',1,'wasm.h']]],
+  ['wasm_5fname_5fdelete_1011',['wasm_name_delete',['../wasm_8h.html#aef3bcc6fc1e0bf9b7708effab82faa33',1,'wasm.h']]],
+  ['wasm_5fname_5fnew_1012',['wasm_name_new',['../wasm_8h.html#aaa2db10e304dbf7a47f741f67d21b1a4',1,'wasm.h']]],
+  ['wasm_5fname_5fnew_5fempty_1013',['wasm_name_new_empty',['../wasm_8h.html#a634115562e4535de51c83381167917ba',1,'wasm.h']]],
+  ['wasm_5fname_5fnew_5fnew_5funinitialized_1014',['wasm_name_new_new_uninitialized',['../wasm_8h.html#a06354acb3a1293b0617c3c4db60bbf7f',1,'wasm.h']]]
+];
---
+++ c-api/search/files_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['wasi_2eh_531',['wasi.h',['../wasi_8h.html',1,'']]],
+  ['wasm_2eh_532',['wasm.h',['../wasm_8h.html',1,'']]],
+  ['wasmtime_2eh_533',['wasmtime.h',['../wasmtime_8h.html',1,'']]]
+];
